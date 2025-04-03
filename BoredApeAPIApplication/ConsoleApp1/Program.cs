19290ffe6eee65f90586c757471664049920e29d
Arlind Kaciku <arlindkaciku18@gmail.com>
2025-04-03 11:00:59 +0200

    A

--- BoredApeAPIApplication/ConsoleApp1/Program.cs
+++ BoredApeAPIApplication/ConsoleApp1/Program.cs
@@ -72,7 +72,7 @@ namespace ConsoleApp1
             public bool Error { get; set; } // to check if there's an error
             public string Category { get; set; } // Joke category
             public string Type { get; set; } // Type of joke (single or twopart)
-            public string joke { get; set; }
+            public string joke { get; set; } 
             public string Setup { get; set; } // The setup of a two-part joke
             public string Delivery { get; set; } // The punchline of a two-part joke
             public bool Safe { get; set; } // Safe joke flag
